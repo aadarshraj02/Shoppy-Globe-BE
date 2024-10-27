b5c853db2801c63f3037dc383c320cad350d8626
adarsh02 <aadarshraazz007@gmail.com>
2024-10-28 03:00:12 +0530

    added route to get all products

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,6 +3,15 @@ import Product from "../model/product.js";
 
 const router = express.Router();
 
+router.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching products", error });
+  }
+});
+
 router.post("/product", async (req, res) => {
   const { title, image, price, description, qty, rating, category, brand } =
     req.body;
