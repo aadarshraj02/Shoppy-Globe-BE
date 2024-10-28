009cd011d49de9985545e00488692b87fc9d3d3e
adarsh02 <aadarshraazz007@gmail.com>
2024-10-28 12:05:49 +0530

    added login user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,3 +21,26 @@ export const registerUser = async (req, res) => {
     return res.status(500).json({ message: "Error registering User", error });
   }
 };
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found please register" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    return res.status(500).json({ message: "Error in Logging in", error });
+  }
+};
